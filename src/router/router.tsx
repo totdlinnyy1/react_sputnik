1d86255ab92cca7f72d69a9dc49331278e3623bc
tot_dlinnyy <gnngr333@gmail.com>
2023-02-10 22:18:04 +0700

    feat: add 404 page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,32 @@
+import {AxiosError, isAxiosError} from 'axios'
 import {createBrowserRouter} from 'react-router-dom'
 
 import App from '../App'
+import NotFound from '../pages/404/notFound'
+import GifService from '../services/gifService/gifService'
+
+import {GifLoaderResponse} from './interfaces'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+    loader: async (): Promise<GifLoaderResponse> => {
+      try {
+        const url = await GifService.fetchGifUrl()
+        return {gif: url}
+      } catch (e) {
+        if (isAxiosError(e)) {
+          const error: AxiosError = e
+          throw new Response(error.message, {status: error.status})
+        }
+        throw e
+      }
+    }
   }
 ])
 
