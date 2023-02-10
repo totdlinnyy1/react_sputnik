4ebed52dfe1b542784cd52d1bf6c755b2a4e6f9a
tot_dlinnyy <gnngr333@gmail.com>
2023-02-10 16:23:48 +0700

    feat: add components library, configure theme config. Add router and specify root route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, {FunctionComponent} from 'react'
+import Header from './components/header/Header'
 
 const App: FunctionComponent = () => {
   return (
-    <></>
+    <>
+      <Header />
+    </>
   )
 }
 
