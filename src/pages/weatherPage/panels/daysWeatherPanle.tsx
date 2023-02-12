babd1799d5b0ac6d1f05011e259217298ec97a84
tot_dlinnyy <gnngr333@gmail.com>
2023-02-12 16:29:21 +0700

    feat: add days weather panel

---
+++ src/pages/weatherPage/panels/daysWeatherPanle.tsx
@@ -0,0 +1,88 @@
+import {
+  Heading,
+  Icon,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack
+} from '@chakra-ui/react'
+import {FC} from 'react'
+import {WiDaySunny} from 'react-icons/wi'
+
+import Weather from '../../../components/weather/Weather'
+
+const DaysWeatherPanel: FC = () => {
+  return (
+    <Tabs isFitted>
+      <TabPanels>
+        <TabPanel>
+          <Weather weather='Облачно' image='cloud.webp' deg={-24} wind={20} />
+        </TabPanel>
+        <TabPanel>
+          <Weather weather='Облачно' image='cloud.webp' deg={-24} wind={20} />
+        </TabPanel>
+        <TabPanel>
+          <Weather weather='Облачно' image='cloud.webp' deg={-24} wind={20} />
+        </TabPanel>
+        <TabPanel>
+          <Weather weather='Облачно' image='cloud.webp' deg={-24} wind={20} />
+        </TabPanel>
+        <TabPanel>
+          <Weather weather='Облачно' image='cloud.webp' deg={-24} wind={20} />
+        </TabPanel>
+      </TabPanels>
+      <TabList>
+        <Tab>
+          <VStack>
+            <Icon as={WiDaySunny} color='yellow' boxSize='10' />
+            <Heading as='h6' size='md'>
+              -24
+            </Heading>
+            <Text>Сегодня</Text>
+          </VStack>
+        </Tab>
+        <Tab>
+          <VStack alignItems='center'>
+            <Icon as={WiDaySunny} color='yellow' boxSize='10' />
+            <Heading as='h6' size='md'>
+              -24
+            </Heading>
+            <Text>Завтра</Text>
+          </VStack>
+        </Tab>
+        <Tab>
+          <VStack alignItems='center'>
+            <Icon as={WiDaySunny} color='yellow' boxSize='10' />
+            <Heading as='h6' size='md'>
+              -24
+            </Heading>
+            <Text>Вторник</Text>
+          </VStack>
+        </Tab>
+        <Tab>
+          <VStack alignItems='center'>
+            <Icon as={WiDaySunny} color='yellow' boxSize='10' />
+            <Heading as='h6' size='md'>
+              -24
+            </Heading>
+            <Text>Среда</Text>
+          </VStack>
+        </Tab>
+        <Tab>
+          <VStack alignItems='center'>
+            <Icon as={WiDaySunny} color='yellow' boxSize='10' />
+            <Heading as='h6' size='md'>
+              -24
+            </Heading>
+            <Text>Четверг</Text>
+          </VStack>
+        </Tab>
+      </TabList>
+    </Tabs>
+  )
+}
+
+export default DaysWeatherPanel
