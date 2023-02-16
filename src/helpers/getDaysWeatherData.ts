19982a20766891814b129914c9f4d497aa0ebe32
tot_dlinnyy <gnngr333@gmail.com>
2023-02-16 15:58:54 +0700

    docs: add comments to new helpers

--- src/helpers/getDaysWeatherData.ts
+++ src/helpers/getDaysWeatherData.ts
@@ -3,9 +3,14 @@ import {DaysWeather} from '../context/weatherContext/weatherContext'
 import {HoursWeatherApiResponse} from './getHoursWeatherData'
 import getReadableData from './getReadableData'
 
+// getDaysWeatherData get weather data from api response and converts it for components
+// 40 weather values come to the input
+// the weather forecast compiled for 5 days is returned
+// if less or more weather comes to the input, then there will be changes in the output
 const getDaysWeatherData = (data: HoursWeatherApiResponse): DaysWeather[] => {
-  const MAX_ELEMENTS = 40
-  const SKIP = 8
+  const MAX_ELEMENTS = data.list.length
+  const DAYS = 5
+  const SKIP = MAX_ELEMENTS / DAYS
   const weather: DaysWeather[] = []
   for (let i = 0; i < MAX_ELEMENTS; i += SKIP) {
     weather.push({
