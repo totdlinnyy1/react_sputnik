61cf5f27eb0e0ee9def2c65b9bd98ca2540a87c8
tot_dlinnyy <gnngr333@gmail.com>
2023-02-16 16:12:18 +0700

    feat: add loading to weather context and handaling errors

--- src/pages/weatherPage/weatherPage.tsx
+++ src/pages/weatherPage/weatherPage.tsx
@@ -1,6 +1,8 @@
 import {
   Box,
+  Center,
   Heading,
+  Spinner,
   Tab,
   TabList,
   TabPanel,
@@ -9,11 +11,33 @@ import {
 } from '@chakra-ui/react'
 import {FC} from 'react'
 
+import {useWeather} from '../../context/weatherContext/weatherContext'
+
 import CurrentWeatherPanel from './panels/currentWeatherPanel'
 import DaysWeatherPanel from './panels/daysWeatherPanle'
 import HoursWeatherPanel from './panels/hoursWeatherPanel'
 
 const WeatherPage: FC = () => {
+  const {isLoading, error} = useWeather()
+
+  if (isLoading) {
+    return (
+      <Center w='full' h='100vh'>
+        <Spinner />
+      </Center>
+    )
+  }
+
+  if (error !== '') {
+    return (
+      <Center w='full' h='50vh'>
+        <Heading as='h1' color='red'>
+          {error}
+        </Heading>
+      </Center>
+    )
+  }
+
   return (
     <Box w='full' mt='8'>
       <Heading as='h1'>Погода</Heading>
