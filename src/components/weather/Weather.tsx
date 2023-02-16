6663634c76be24845536b7a324623a873366c91f
tot_dlinnyy <gnngr333@gmail.com>
2023-02-16 15:15:24 +0700

    feat: show real weather on hours weather panel
    Now hours weather panel get real weather data, also get icons from api and main weather image
    choosing by weather id

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -1,17 +1,28 @@
 import {Box, Heading, Image, Text} from '@chakra-ui/react'
 import {FC} from 'react'
 
+import capitalizeFirstLetter from '../../helpers/capitalizeFirstLetter'
+import getRoundedTemp from '../../helpers/getRoundedTemp'
+
 interface Props {
-  weather: string
   image: string
-  deg: number
   wind: number
+  temp: number
+  pressure: number
+  humidity: number
+  description: string
 }
 const Weather: FC<Props> = props => {
   return (
     <Box w='full'>
       <Box pos='relative'>
-        <Image src={props.image} alt={props.weather} />
+        <Image
+          w='full'
+          h='530px'
+          objectFit='cover'
+          src={props.image}
+          alt={props.description}
+        />
         <Box
           bgColor='blackAlpha.400'
           pos='absolute'
@@ -21,9 +32,12 @@ const Weather: FC<Props> = props => {
           p={1}
         >
           <Heading as='h3'>
-            {props.deg}°C {props.weather}
+            {getRoundedTemp(props.temp)}°C{' '}
+            {capitalizeFirstLetter(props.description)}
           </Heading>
           <Text>Ветер {props.wind} м/с</Text>
+          <Text>Влажность {props.humidity}%</Text>
+          <Text>Давление {props.pressure} мбар</Text>
         </Box>
       </Box>
     </Box>
