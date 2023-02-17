0e01b0a238e91c27cc7b77722ef3bf95efb6f102
tot_dlinnyy <gnngr333@gmail.com>
2023-02-17 17:09:32 +0700

    feat: add gallery page
    Gallery page with gallery context, get random photo and search photo by query

---
+++ src/context/galleryContext/galleryContext.tsx
@@ -0,0 +1,92 @@
+import axios, {isAxiosError} from 'axios'
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
+
+const ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY || ''
+
+interface GalleryContextProps {
+  isLoading: boolean
+  error: string
+  randomPhoto?: Photo
+  photos?: Photo[]
+}
+
+interface User {
+  name: string
+}
+
+interface Urls {
+  full: string
+  small: string
+}
+
+interface SearchResult {
+  results: Photo[]
+}
+
+export interface Photo {
+  user: User
+  description?: string
+  alt_description: string
+  urls: Urls
+}
+
+const GalleryContext = createContext<GalleryContextProps>({
+  isLoading: true,
+  error: ''
+})
+
+interface ProviderProps {
+  children: ReactNode
+}
+
+export const GalleryContextProvider: FC<ProviderProps> = ({children}) => {
+  const [randomPhoto, setRandomPhoto] = useState<Photo>()
+  const [photos, setPhotos] = useState<Photo[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string>('')
+
+  const randomPhotoApi = `https://api.unsplash.com/photos/random?client_id=${ACCESS_KEY}`
+  const searchApi = `https://api.unsplash.com/search/photos?client_id=${ACCESS_KEY}&query=Moskow`
+
+  useEffect(() => {
+    setIsLoading(true)
+    const fetchData = async (): Promise<void> => {
+      try {
+        const randomPhotoResponse = await axios<Photo>(randomPhotoApi)
+        setRandomPhoto(randomPhotoResponse.data)
+        const searchPhotoResponse = await axios<SearchResult>(searchApi)
+        setPhotos(searchPhotoResponse.data.results)
+        setIsLoading(false)
+      } catch (e) {
+        if (isAxiosError(e)) {
+          setError(
+            'Что-то определенно пошло не так! Надеюсь это у api что-то накрылось, а не у меня.'
+          )
+        }
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <GalleryContext.Provider value={{randomPhoto, photos, isLoading, error}}>
+      {children}
+    </GalleryContext.Provider>
+  )
+}
+
+export const useGallery = (): GalleryContextProps => {
+  const context = useContext(GalleryContext)
+  if (context === undefined) {
+    throw new Error('Context must be used within a Provider')
+  }
+  return context
+}
